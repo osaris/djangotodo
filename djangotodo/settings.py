bd29dad3cdc4fc156a316bc179a489ef82b2ec61
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2023-03-12 17:58:41 +0100

    Use django-environ to store config in env. var

--- djangotodo/settings.py
+++ djangotodo/settings.py
@@ -10,20 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import environ
+import os
+
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
 
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--9+i&a3%rr25y-$du79@j@0wz=#g$bq74^6^f3ha64j&qc!s%1'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -75,14 +82,7 @@ WSGI_APPLICATION = 'djangotodo.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangotodo', 
-        'USER': 'djangotodo', 
-        'PASSWORD': 'djangotodo',
-        'HOST': '172.30.64.1',
-        'PORT': '5432',
-    }
+    'default': env.db(),
 }
 
 
